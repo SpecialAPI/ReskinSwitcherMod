922d3bb0d05b8a84076ed0530beb368ff3fa2c91
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-05-19 11:20:03 +0300

    Bump plugin version

--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ namespace ReskinSwitcherMod
     {
         public const string GUID = "spapi.etg.reskinswitcher";
         public const string NAME = "Reskin Switcher";
-        public const string VERSION = "1.1.0";
+        public const string VERSION = "1.2.1";
 
         public void Awake()
         {
